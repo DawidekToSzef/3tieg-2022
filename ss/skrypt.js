366fce8400ed5de3eb30335e3537bd5d4d6bcdc2
DawidekToSzef <97887142+DawidekToSzef@users.noreply.github.com>
2022-05-11 16:08:56 +0200

    Add files via upload

--- ss/skrypt.js
+++ ss/skrypt.js
@@ -1,12 +1,37 @@
-function euklides(){
-    let a=parseInt(document.getElementById("liczbaA").value);
-    let b=parseInt(document.getElementById("liczbaB").value);
-while(a!==b){
-    if(a>b){
-        a=a-b;
-    }else{
-        b=b-a;
+function obejrz(){
+    let a=parseInt(document.getElementById("jeden").value)
+    let b=parseInt(document.getElementById("dwa").value)
+    while(a!==b){
+        if(a>b){
+            a=a-b
+        }
+        else{
+            b=b-a
+        }
     }
+    document.getElementById("euklidesWynik").value=a
 }
-document.getElementById("euklidesWynik").value=a;
+function obejrz2(a,b){
+    while(a!==b){
+        if(a>b){
+            a=a-b
+        }
+        else{
+            b=b-a
+        }
+    }
+    return a
+}
+function nww(){
+    let a=parseInt(document.getElementById("jedenNWW").value)
+    let b=parseInt(document.getElementById("dwaNWW").value)
+    let x=a*b
+    while(b!==0){
+        let c=a%b
+        a=b
+        b=c
+    }
+        let NWD=a
+        let wynik = x/NWD
+        document.getElementById("nwwWynik").value=wynik
 }
